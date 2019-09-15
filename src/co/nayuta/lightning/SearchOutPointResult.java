693a3283f952f8fea525240d4016eb373f55c0a9
ueno <ueno@nayuta.co>
2019-09-15 12:35:23 +0900

    revise

---
+++ src/co/nayuta/lightning/SearchOutPointResult.java
@@ -0,0 +1,12 @@
+package co.nayuta.lightning;
+
+public class SearchOutPointResult {
+    int height;
+    byte[] tx;
+    //
+    //
+    SearchOutPointResult() {
+        this.height = 0;
+        this.tx = null;
+    }
+}
